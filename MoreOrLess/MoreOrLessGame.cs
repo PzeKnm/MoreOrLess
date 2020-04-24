42877b180f8be08f85a2e9f4dc28054cc74c6240
PzeKnm <martin.knott@gmx.ch>
2020-04-25 08:37:40 +1000

    Add project files.

---
+++ MoreOrLess/MoreOrLessGame.cs
@@ -0,0 +1,387 @@
+﻿using GameLib;
+using Newtonsoft.Json;
+using RaspberryGPIOManager;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace MoreOrLess
+{
+
+  public class MoreOrLess: Game
+  {
+
+    // Constants
+    int _cGameLengthSecs = 60;
+    int _cAuthenticationtimeoutSecs = 30;
+
+
+    int _cShowAnswerSecs = 5;
+    int _cShowAutoAcceptAnswerSecs = 20;
+
+    // How often to update viz regardless of detected changes.
+    int _cUpdateVisualisationMSecs = 1000;
+
+    public enum InternalState
+    {
+      Ready = 0,
+      FetchQuestion,
+      DisplayQuestion,
+      DisplayAnswer
+    }
+
+    InternalState _internalState;
+    
+    QuestionSource _questionSource;
+
+    Question _currentQuestion;
+
+    AnswerParameters _currentAnswer;
+
+
+    Watchdog _dogShowAnswerTimeout;
+    Watchdog _dogAcceptAnswerTimeout;
+    Watchdog _dogUpdateVisualisation;
+    Watchdog _dogAuthenticationCountdownUpdate;
+
+    VisualisationGenerator _visGenerator;
+
+    RaspPi _pi;
+
+
+
+
+    public MoreOrLess(GameManager mgr) : base(mgr)
+    {
+      Console.WriteLine("Program 'MoreOrLess' begins.");
+      _visGenerator = new VisualisationGenerator();
+
+      SetHubDeviceDetails("Station002", "rgvlStnhl3c7vPMeFI9OEK5TnQGL/0WNPBgZji/Hiro=");
+      SetGameLength(_cGameLengthSecs);
+      SetAuthenticationTimeoutSec(_cAuthenticationtimeoutSecs);
+      _HeartbeatSec = 20;
+      
+      _dogShowAnswerTimeout = new Watchdog(1000 * _cShowAnswerSecs);// 
+      _dogShowAnswerTimeout.WatchdogBites += _dogShowAnswerTimeout_WatchdogBites;
+      _dogShowAnswerTimeout.Start();
+
+      _dogAcceptAnswerTimeout = new Watchdog(1000 * _cShowAutoAcceptAnswerSecs);// 
+      _dogAcceptAnswerTimeout.WatchdogBites += _dogAcceptAnswerTimeout_WatchdogBites;
+      _dogAcceptAnswerTimeout.Start();
+
+      _dogUpdateVisualisation = new Watchdog(_cUpdateVisualisationMSecs);// 
+      _dogUpdateVisualisation.WatchdogBites += _dogUpdateVisualisation_WatchdogBites;
+      _dogUpdateVisualisation.Start();
+
+      _dogAuthenticationCountdownUpdate = new Watchdog(800);// 800ms
+      _dogAuthenticationCountdownUpdate.WatchdogBites += _dogAuthenticationCountdown_WatchdogBites;
+      _dogAuthenticationCountdownUpdate.Stop();
+
+      SetInternalState(InternalState.Ready);
+    }
+
+    private void SetInternalState(InternalState newState)
+    {
+      _internalState = newState;
+      UpdateVisualisation();
+    }
+
+    private InternalState GetInternalState()
+    {
+      return _internalState;
+    }
+
+         
+
+    public override void ProcessGameCommand(GameCommand gc)
+    {
+      if(GetInternalState() != InternalState.DisplayQuestion)
+        return;
+
+      if (gc.Command == "DecLo")
+      {
+        _currentAnswer.DecrementLo();
+        DisplayOutput();        
+      }
+        
+      if (gc.Command == "IncLo")
+      {
+        _currentAnswer.IncrementLo();
+        DisplayOutput();        
+      }   
+
+      if (gc.Command == "DecHi")
+      {
+        _currentAnswer.DecrementHi();
+        DisplayOutput();        
+      }
+
+      if (gc.Command == "IncHi")
+      {
+        _currentAnswer.IncrementHi();
+        DisplayOutput();        
+      }
+
+      if (gc.Command == "AcceptAnswer")
+      {
+        AcceptAnswer();
+        DisplayOutput();        
+      }
+      /*
+      if (gc.Command == "Left")
+        BumpLeft();
+      if (gc.Command == "Right")
+        BumpRight();
+        */
+    }
+
+
+    public override void Initialise()
+    {
+      if(_questionSource == null)
+        _questionSource = new QuestionSource(GetRestApi());
+      
+      bool bSim = false;
+      #if( SIMULATION )
+            bSim = true;
+      #endif 
+      
+      _pi = new RaspPi(bSim);
+      _pi.Initialise();
+
+      GetRestApi().ShowPerformance(false);
+    }
+    
+    public override void Deinitialise()
+    {
+      _pi.Deinitialise();
+    }
+
+    public override string GetGameCommands()
+    {
+      string s = "";
+      s += "Q : DecLo" + Environment.NewLine;
+      s += "W : IncLo" + Environment.NewLine;
+      s += "E : DecHi" + Environment.NewLine;
+      s += "R : IncHi" + Environment.NewLine;
+      s += "T : AcceptAnswer" + Environment.NewLine;
+
+      return s;
+    }
+
+    public override bool ProcessConsoleCommand(ConsoleKeyInfo k)
+    {
+      if(GetGameMangerState() != GameManagerState.GamePlaying)
+        return false;
+
+      if (k.Key == ConsoleKey.Q)
+      {
+        ProcessGameCommand(new GameCommand("DecLo", ""));        
+        return true;
+      }
+
+      if (k.Key == ConsoleKey.W)
+      {
+        ProcessGameCommand(new GameCommand("IncLo", ""));        
+        return true;
+      }
+
+      if (k.Key == ConsoleKey.E)
+      {
+        ProcessGameCommand(new GameCommand("DecHi", ""));        
+        return true;
+      }
+
+      if (k.Key == ConsoleKey.R)
+      {
+        ProcessGameCommand(new GameCommand("IncHi", ""));       
+        return true;
+      }
+
+      if (k.Key == ConsoleKey.T)
+      {
+        AcceptAnswer();
+        return true;
+      }
+            
+      return false;
+    }   
+    
+
+    public override void GameStateHasChanged() 
+    { 
+      UpdateVisualisation();
+
+      if(GetGameMangerState() == GameManagerState.Authenticating)
+        _dogAuthenticationCountdownUpdate.Start();
+      else
+        _dogAuthenticationCountdownUpdate.Stop();
+    }
+
+
+    public override void ChangeToPreGame()
+    {
+      base.ChangeToPreGame();
+    }
+
+    public override void ChangeToBeginGame() 
+    {      
+      base.ChangeToBeginGame();
+
+      if(GetInternalState() == InternalState.Ready)
+      {
+        StartEndOfGameWatchdog();
+        FetchQuestion();
+      }
+    }
+
+    
+    public override void ChangeToEndGame() 
+    { 
+      // overridden so that game doesn't end immediately; the user should have a chance to
+      // answer current question.
+    }
+
+
+    private void FetchQuestion()
+    {
+      SetInternalState(InternalState.FetchQuestion);
+      _currentQuestion = _questionSource.GetQuestion();      
+      _currentAnswer = new AnswerParameters();
+      _currentAnswer.SetOriginalRange(_currentQuestion.Answer, _currentQuestion.RangeLo, _currentQuestion.RangeHi);
+      
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("");
+      Console.WriteLine("------------------------------------------------------------------");
+      Console.WriteLine("New Question      -    remaining time(s): " + GetRemainingGameTimeSec());
+      Console.WriteLine("------------");
+           
+      _dogAcceptAnswerTimeout.Reset();
+      DisplayOutput();
+      SetInternalState(InternalState.DisplayQuestion);
+    }
+
+    private void _dogAcceptAnswerTimeout_WatchdogBites(object sender, ElapsedEventArgs e)
+    {
+      if(GetInternalState() != InternalState.DisplayQuestion)
+        return;
+      AcceptAnswer();
+    }
+
+
+    private void AcceptAnswer()
+    {
+      // _internalState = InternalState.DisplayAnswer;      
+
+      Console.WriteLine("The question was:");
+      Console.WriteLine(_currentQuestion.QuestionText);
+      Console.WriteLine("Your answer was: " + _currentAnswer.GetRangeAsString());      
+      Console.WriteLine("The correct answer is:" + _currentQuestion.Answer );
+      if(_currentAnswer.IsInRange(_currentQuestion.Answer))
+      {
+        Console.WriteLine("Yay!");
+        PerformPiAnswer(true);
+        IncreaseScore(_currentAnswer.GetPotentialPoints());
+      }
+      else
+      {
+        Console.WriteLine("Doh!");
+        PerformPiAnswer(false);
+      }
+
+      
+      SetInternalState(InternalState.DisplayAnswer);
+      _dogShowAnswerTimeout.Reset();
+
+      Console.WriteLine("         Current Score: {0}", GetScore());  
+    }
+
+    private void _dogShowAnswerTimeout_WatchdogBites(object sender, ElapsedEventArgs e)
+    {
+      if(GetInternalState() != InternalState.DisplayAnswer)
+        return;
+
+      if(HasEndOfGameFlagged())
+      {
+        SetInternalState(InternalState.Ready);
+        InformManagerGameFinished();
+        _questionSource.Reset();
+      }
+      else
+      {
+        Console.WriteLine("Next question.....");
+        FetchQuestion();
+      }
+
+    }
+
+
+    private void _dogUpdateVisualisation_WatchdogBites(object sender, ElapsedEventArgs e)
+    {
+      UpdateVisualisation();
+    }
+
+    private void _dogAuthenticationCountdown_WatchdogBites(object sender, ElapsedEventArgs e)
+    {
+      UpdateVisualisation();
+      _dogAuthenticationCountdownUpdate.Reset();
+    }
+
+    private void DisplayOutput()
+    {
+      Console.WriteLine(_currentQuestion.QuestionText);
+      _currentAnswer.DisplayOnConsole();
+
+      UpdateVisualisation();
+    }
+
+    private void UpdateVisualisation()
+    {
+      int nRemainingTime = GetRemainingGameTimeSec();
+      int nTotalTime = _cGameLengthSecs;
+
+      if(GetGameMangerState() == GameManagerState.PreGame)
+      {
+        nRemainingTime = GetRemainingPreGameTimeSec();
+        nTotalTime = GetTotalPreGameTimeSec();
+      }
+      
+      if(GetGameMangerState() == GameManagerState.Authenticating)
+      {
+        nRemainingTime = GetRemainingAthenticationSec();
+        nTotalTime = GetTotalAthenticationSec();
+      }
+
+      if(GetGameMangerState() == GameManagerState.PostGame)
+      {
+        nRemainingTime = GetRemainingPostGameTimeSec();
+        nTotalTime = GetTotalPostGameTimeSec();
+      }
+      
+      int nRemainingQuestionTime = 0;
+      if(/*_dogAcceptAnswerTimeout != null*/GetInternalState() == InternalState.DisplayQuestion)
+        nRemainingQuestionTime = _dogAcceptAnswerTimeout.GetRemainingTimeSec();
+
+      _visGenerator.UpdateVisualisation(GetGameMangerState(), GetInternalState(), GetAccessCode(),
+        _currentQuestion, _currentAnswer, GetScore(), 
+        _cGameLengthSecs, nRemainingTime, nRemainingQuestionTime);
+
+      NotifyClientTimeRemaining();
+      _dogUpdateVisualisation.Reset();
+    }
+
+    private void PerformPiAnswer(bool bCorrect)
+    {
+      int nPulseMs = 1000 * _cShowAnswerSecs;
+      if(bCorrect)
+        _pi.PulsePin(GPIOPinDriver.Pin.GPIO17, nPulseMs);
+      else
+        _pi.PulsePin(GPIOPinDriver.Pin.GPIO27, nPulseMs);
+    }
+  }
+
+
+
+}
---
+++ MoreOrLess/VisualisationI2CServer.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Device.I2c;
+
+namespace MoreOrLess
+{
+
+  public struct SVizData {
+
+    public int GameState;
+    public int InternalState;
+    public int PotentialScore;
+    public int Score;
+    public int TotalGameSecs;
+    public int RemainingSecs;
+    public int RemainingQuestionSecs;
+  };
+
+  public class I2CSlaveData
+  {
+    public I2CSlaveData(byte addr, string name)
+    {
+      m_Address = addr;
+      m_Name = name;
+      m_bOnline = false;
+    }
+
+    public byte m_Address;
+    public string m_Name;
+    public bool m_bOnline;
+  }
+
+  class VisualisationI2CServer
+  {
+    private int m_cBroadcastAddr = 0x0;
+
+
+    private List<I2CSlaveData> m_PotentialSlaves;
+
+    public VisualisationI2CServer()
+    {
+      m_PotentialSlaves = new List<I2CSlaveData>();
+
+      // Even though we're going to broadcast the data to anyone who's listening, it would still
+      // be nice to be able to test the bus for expected slaves.
+
+      // todo, maybe read these from a config file...
+      m_PotentialSlaves.Add(new I2CSlaveData(0x2b, "Potential Score Marker"));
+      m_PotentialSlaves.Add(new I2CSlaveData(0x2d, "Soemthing else"));
+
+      CheckBus();
+    }
+         
+    public void PublishVisualisationData(VisualisationData vd)
+    {      
+      SVizData data;
+      data.GameState = vd.GameStateInt;
+      data.InternalState = vd.InternalStateInt;
+      data.PotentialScore = vd.PotentialScore;
+      data.Score = vd.Score;
+      data.TotalGameSecs = vd.TotalGameSecs;
+      data.RemainingSecs = vd.RemainingSecs;
+      data.RemainingQuestionSecs = vd.RemainingQuestionSecs;
+
+      // Broadcast to all the devices on the bus and send them all the good news.
+      var i2cDevice = I2cDevice.Create(new I2cConnectionSettings(busId: 1, deviceAddress: m_cBroadcastAddr));
+      var sm = new I2CSlaveDevice(i2cDevice);
+      try
+      {
+        sm.SendVisualisationData(data);
+      }
+      catch
+      {
+      }
+
+      sm.Dispose();
+    }
+
+
+    private void CheckBus()
+    {
+      SVizData data;
+      data.GameState = 0;
+      data.InternalState = 0;
+      data.PotentialScore = 0;
+      data.Score = 0;
+      data.TotalGameSecs = 0;
+      data.RemainingSecs = 0;
+      data.RemainingQuestionSecs = 0;
+
+      foreach(I2CSlaveData slave in m_PotentialSlaves)
+      {
+        var i2cDevice = I2cDevice.Create(new I2cConnectionSettings(busId: 1, deviceAddress: slave.m_Address));
+        var sm = new I2CSlaveDevice(i2cDevice);
+        try
+        {
+          sm.SendVisualisationData(data);
+          slave.m_bOnline = true;
+          Console.WriteLine("Device: " + slave.m_Name + " OK at address: " + slave.m_Address.ToString());
+        }
+        catch
+        {
+          Console.WriteLine("Device: " + slave.m_Name + " not contactable at address: " + slave.m_Address.ToString());
+        }
+
+        sm.Dispose();
+      }			
+    }
+  }
+
+  /*
+    private void CheckBusOld()
+    {
+      SVizData data;
+      data.GameState = 0;
+      data.InternalState = 0;
+      data.PotentialScore = 0;
+      data.Score = 0;
+      data.TotalGameSecs = 0;
+      data.RemainingSecs = 0;
+      data.RemainingQuestionSecs = 0;
+
+      foreach(I2CSlaveData slave in m_Slaves)
+      {
+        var i2cDevice = I2cDevice.Create(new I2cConnectionSettings(busId: 1, deviceAddress: slave.m_Address));
+        var sm = new I2CSlaveDevice(i2cDevice);
+        try
+        {
+          sm.SendVisualisationData(data);
+          slave.m_bOnline = true;
+          Console.WriteLine("Device: " + slave.m_Name + " OK at address: " + slave.m_Address.ToString());
+        }
+        catch
+        {
+          Console.WriteLine("Device: " + slave.m_Name + " not contactable at address: " + slave.m_Address.ToString());
+        }
+
+        sm.Dispose();
+      }			
+    }
+  */
+
+  class I2CSlaveDevice: IDisposable
+  {
+      public const byte I2cAddressBase = 0x2b;
+
+    private I2cDevice _device;
+
+    public I2CSlaveDevice(I2cDevice i2cDevice)
+    {
+      _device = i2cDevice;
+    }
+
+
+    public void SendVisualisationData(SVizData data)
+    {
+      int size = 7;
+      var arr = new byte[size];
+      
+      arr[0] = (byte)data.GameState;
+      arr[1] = (byte)data.InternalState;
+      arr[2] = (byte)data.PotentialScore;
+      arr[3] = (byte)data.Score;
+      arr[4] = (byte)data.TotalGameSecs;
+      arr[5] = (byte)data.RemainingSecs;
+      arr[6] = (byte)data.RemainingQuestionSecs;
+           
+      ReadOnlySpan<byte> bytes = arr; // Implicit cast from T[] to Span<T>
+      _device.Write(bytes);
+    }
+
+
+
+    public void Dispose()
+    {
+      _device?.Dispose();
+      _device = null;
+    }
+  }
+
+
+
+
+}
